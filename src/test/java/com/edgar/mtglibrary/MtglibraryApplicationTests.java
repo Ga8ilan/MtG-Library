c00b85e2c844a5234e10462abefc7cd3d2de1219
Edgar Castaneda <castaneda.e@northeastern.edu>
2026-04-13 12:51:07 -0400

    setup

---
+++ src/test/java/com/edgar/mtglibrary/MtglibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgar.mtglibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtglibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
